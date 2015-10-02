74f0497b8611789e932f588b20f4896e1d8f5497
FlossyWeb <flossyweb@gmail.com>
2015-10-02 02:09:00 +0200

    Up to v 1.6.0 + chenge url to open business app and be back...

--- js/home.js
+++ js/home.js
@@ -775,7 +775,7 @@ function myTaxiDown()
 {
 	//var url = "http://www.taximedia.fr/stores.php?app=pro";
 	//window.open(url,'_blank','location=yes,enableViewportScale=yes,closebuttoncaption=Fermer');
-	window.open('montaxipro://', '_system');
+	window.open('montaxipro://?from=driver', '_system');
 }
 function Share()
 {
