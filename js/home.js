9399eecdbb31acd769ec358be50841100c2f5a7d
FlossyWeb <flossyweb@gmail.com>
2016-06-02 19:22:36 +0200

    Exactly the same...

--- js/home.js
+++ js/home.js
@@ -490,7 +490,7 @@ function update()
 		cache: false,
         timeout: 5000 // in milliseconds
     }).done(function(data) {
-		$('body').append('Done !!<br>');
+		$('#screen_job').append('Done !!<br>');
 		if (data != 0)
 		{
 			$("#screen_job").empty().append(data);
@@ -527,7 +527,7 @@ function update()
 			});
 		}
 	}).always(function(data) {
-		$('body').append('Complete anyway !!<br>');
+		$('#screen_job').append('Complete anyway !!<br>');
 		setTimeout('update()', pollingTime);
 	});
 }
